39cf3ba08b27fcde7271930897408b13c2b9aa43
komalkknk <78220140+komalkknk@users.noreply.github.com>
2021-09-28 23:01:18 +0530

    Update settings.py

--- komal_nitin_kale_inventory_management_system/settings.py
+++ komal_nitin_kale_inventory_management_system/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^rnp-jk!%!b*swidv5b_by@zki7osvjr783^8=c6!ht&ty#k$6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
